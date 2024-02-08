cbe5c8c18884b49481079f1c607a1d593bf2690f
hugob1234 <159055356+hugob1234@users.noreply.github.com>
2024-02-08 22:35:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ option = input("Enter option (1 for adding record, 2 for removing record, 3 for 
 n = int(input(f"How many pieces of data would you like to add/remove/check using option {option}: "))
 while ((option != "1") and (option != "2") and (option != "3")):
     option = input("Option picked not valid , Enter option (1 for adding record, 2 for removing record, 3 for checking if your record is in database): ")
-    n = int(input(f"Enter how many pieces of data would you like to add/remove/check using option {option}: "))
+    n = int(input(f"Enter how many pieces of data you would like to add/remove/check using option {option}: "))
 
 if option == "1":
     for i in range(n):
