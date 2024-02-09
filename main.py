9c0d8386e6737dd6da3eaa6e24dafae975b9a2b6
hugob1234 <159055356+hugob1234@users.noreply.github.com>
2024-02-09 22:20:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -14,35 +14,35 @@ def check(name, age, address):
                 break
         else:
             print("Record not found")
-
-
-
 print("File manager")
-option = input("Enter option (add for adding record, remove for removing record, check for checking if your record is in database): ")
-n = int(input(f"How many pieces of data would you like to {option}: "))
-while ((option != "add") and (option != "remove") and (option != "check")):
-    option = input("Option picked not valid , Enter option (add for adding record, remove for removing record, check for checking if your record is in database): ")
-    n = int(input(f"Enter how many pieces of data would you like to {option}: "))
-
-if option == "add":
-    for i in range(n):
-        qu = str(input("Enter your full name: "))
-        age = int(input("Enter age: "))
-        last = str(input("Enter postcode: "))
-        with open("data.txt", "a") as file:
-            file.write(f"full name: {qu}   age: {age}   address: {last}\n")
-    print(f"successfully added data.")
-elif option == "remove":
-    for i in range(n):
-        qu = str(input("Enter your full name: "))
-        age = int(input("Enter age: "))
-        last = str(input("Enter postcode: "))
-        remove_record(qu, age, last)
-elif option == "check":
-    for i in range(n):
-        qu = str(input("Enter your full name: "))
-        age = int(input("Enter age: "))
-        last = str(input("Enter postcode: "))
-        check(qu, age, last)
-else:
-    print("Error")
+while True:
+    option = input("Enter option add for adding record, remove for removing record, check for checking if your record is in database, or exit to quit :")
+    if option == "exit":
+        break
+    n = int(input(f"How many pieces of data would you like to {option}: "))
+    while ((option != "add") and (option != "remove") and (option != "check")):
+        option = input("Option picked not valid , Enter option (add for adding record, remove for removing record, check for checking if your record is in database, or exit to quit): ")
+        n = int(input(f"Enter how many pieces of data would you like to {option}: "))
+    if option == "add":
+        for i in range(n):
+            qu = str(input("Enter your full name: "))
+            age = int(input("Enter age: "))
+            last = str(input("Enter address: "))
+            with open("data.txt", "a") as file:
+                file.write(f"full name: {qu}   age: {age}   address: {last}\n")
+        print("successfully added data.")
+    elif option == "remove":
+        for i in range(n):
+            qu = str(input("Enter your full name: "))
+            age = int(input("Enter age: "))
+            last = str(input("Enter address: "))
+            remove_record(qu, age, last)
+    elif option == "check":
+        for i in range(n):
+            qu = str(input("Enter your full name: "))
+            age = int(input("Enter age: "))
+            last = str(input("Enter address: "))
+            check(qu, age, last)
+            print("check completed")
+    else:
+        print("Error")    
